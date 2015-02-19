e76ec810a305eb8eb2da40a5a1c64ffefe9ad04e
Dieter Beelaert <dieterbeelaert3@msn.com>
2015-02-19 13:33:23 +0100

    openshift support

--- server.js
+++ server.js
@@ -15,5 +15,11 @@ server.all('*',function(req,res){
     route.routeRequest(req,res);
 });
 
-server.listen(settings.port);
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+server.listen(port,ip,function(){
+    console.log('moviewatcher server started successfully');
+});
+
  //run it on openshift https://github.com/openshift-quickstart/openshift-mongo-node-express-example/blob/master/server.js
